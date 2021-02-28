e584a65aea58f62be9048161c22c962b468a9b50
Jake Huang <yiju@umich.edu>
2021-02-27 21:43:32 -0500

    add bundle option (remove GenModel.js in output)

--- bash/mainSep.sh
+++ bash/mainSep.sh
@@ -9,9 +9,11 @@ mv $1 src/main/java &&							# move POJO into src/main/java
 bash/pojoFilter.sh src/main/java/$1 &&					# make POJO file h2o-genmodel independent
 bash/pojoFilter.sh src/main/java/GenModel.java &&			# make GenModel h2o independent
 mvn generate-sources;							# make sure it is OK if JSweet exits on error(;)
-mv target/js/bundle.js target/js/$jsOut &&				# rename bundle.js to JS file corresponding to pojo name
-bash/jsweetFilter.sh target/js/$jsOut &&				# filter left-over Java syntax
-bash/exports.sh target/js/$jsOut >> target/js/$jsOut &&			# export JS class
+bash/jsweetFilter.sh target/js/$jsOut &&				# clean up JSweet left-over Java code
+bash/jsweetFilter.sh target/js/GenModel.js &&				# clean up JSweet left-over Java code
+bash/exports.sh target/js/$jsOut >> target/js/$jsOut &&			# export JS-converted POJO as a class
+bash/exports.sh target/js/GenModel.js >> target/js/GenModel.js &&	# export GenModel
+bash/import.sh target/js/GenModel.js target/js/$jsOut &&		# import GenModel to JS-converted POJO
 mv target/js/* "$cwd" && 						# move all JS files into original directory
 rm -rf target/ &&							# remove target directory
 rm src/main/java/* && 							# clean up src/main/java
